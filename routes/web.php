46d5400773f2460b345e6d74f4f008ebdcd5fcca
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-03-20 14:32:17 +0100

    train route management delegated to the controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\train\TrainController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
   return view('home');
 })->name('home');
 
-Route::get('/train', function () {
-  return view('train.index');
-})->name('train.index');
+Route::get('/train', [TrainController::class, 'index'])->name('train.index');
