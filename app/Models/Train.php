c5ce88d24ce26d9f14402b79fd01445eec6392d6
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-03-20 22:25:27 +0100

    add dateTime gettern on train model

--- app/Models/Train.php
+++ app/Models/Train.php
@@ -8,4 +8,37 @@ use Illuminate\Database\Eloquent\Model;
 class Train extends Model
 {
   // use HasFactory;
+
+  private function formatDate($date)
+  {
+    $date = str_replace('/', '-', $date);
+    $date = date('d-m-y', strtotime($date));
+    return $date;
+  }
+
+  private function formatTime($time)
+  {
+    $time = date("g:i", strtotime($time));
+    return $time;
+  }
+  public function getDepDateTime()
+  {
+    $date = $this->formatDate($this->departure_date);
+    $time = $this->formatTime($this->departure_time);
+
+    return "[$date] " . $time;
+  }
+
+  public function getArrDateTime()
+  {
+    $dep_date = $this->formatDate($this->departure_date);
+    $arr_date = $this->formatDate($this->arrival_date);
+    $arr_time = $this->formatTime($this->arrival_time);
+
+    if ($dep_date != $arr_date) {
+      return "[$arr_date] " . $arr_time;
+    } else {
+      return $arr_time;
+    }
+  }
 }
