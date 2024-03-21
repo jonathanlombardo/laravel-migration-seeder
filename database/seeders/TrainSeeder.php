615d2335ffe50f1bd7b496f6d41d6ae113d2e193
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-03-21 17:36:23 +0100

    add Faker generation to Train seeder

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\Train;
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class TrainSeeder extends Seeder
 {
@@ -13,21 +15,37 @@ class TrainSeeder extends Seeder
    *
    * @return void
    */
-  public function run()
+  public function run(Faker $faker)
   {
-    $train = new Train();
-    $train->company = "Company";
-    $train->station_from = "Garbagnate";
-    $train->station_to = "Centrale";
-    $train->departure_date = "2023-03-24";
-    $train->departure_time = "10:00:00";
-    $train->arrival_date = "2023-03-24";
-    $train->arrival_time = "15:00:00";
-    // $train->code = "";
-    $train->wagons = "12";
-    $train->on_time = "1";
-    $train->cancelled = "0";
-    $train->save();
+    $existingCode = Train::whereRaw("code IS NOT NULL")->get()->pluck("code")->toArray();
+
+    for ($i = 0; $i < 100; $i++) {
+
+      $departDateObj = $faker->dateTimeBetween('-1 week', '+1 week');
+      $departDate = date('Y-m-d', $departDateObj->getTimestamp());
+      $departTime = date('H:i:s', $departDateObj->getTimestamp());
+
+      $dt = Carbon::createFromFormat('Y-m-d H:i:s', "$departDate $departTime");
+
+      $arrDateObj = $dt->addHours($faker->numberBetween(1, 48));
+      $arrDate = date('Y-m-d', $arrDateObj->getTimestamp());
+      $arrTime = date('H:i:s', $arrDateObj->getTimestamp());
+
+      $train = new Train();
+
+      $train->company = $faker->company();
+      $train->station_from = "{$faker->city()} Station {$faker->randomDigitNotNull()}";
+      $train->station_to = "{$faker->city()} Station {$faker->randomDigitNotNull()}";
+      $train->departure_date = $departDate;
+      $train->departure_time = $departTime;
+      $train->arrival_date = $arrDate;
+      $train->arrival_time = $arrTime;
+      // $train->code = "";
+      $train->wagons = $faker->numberBetween(5, 30);
+      $train->on_time = $faker->numberBetween(0, 1);
+      $train->cancelled = $faker->numberBetween(0, 1);
+      $train->save();
+    }
 
   }
 }
