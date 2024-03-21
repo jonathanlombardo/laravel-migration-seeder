74a55c2b1c77bb0c6433673ce4f8bc757bef4b7c
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-03-21 22:10:50 +0100

    fix: train seeder - add unique code

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -21,15 +21,21 @@ class TrainSeeder extends Seeder
 
     for ($i = 0; $i < 100; $i++) {
 
+      // generate departure (+- 1week from now) date
+      // and arrival date (+ 1h-24h from departure)
       $departDateObj = $faker->dateTimeBetween('-1 week', '+1 week');
       $departDate = date('Y-m-d', $departDateObj->getTimestamp());
       $departTime = date('H:i:s', $departDateObj->getTimestamp());
-
       $dt = Carbon::createFromFormat('Y-m-d H:i:s', "$departDate $departTime");
-
-      $arrDateObj = $dt->addHours($faker->numberBetween(1, 48));
+      $arrDateObj = $dt->addHours($faker->numberBetween(1, 24));
       $arrDate = date('Y-m-d', $arrDateObj->getTimestamp());
       $arrTime = date('H:i:s', $arrDateObj->getTimestamp());
+
+      // generate code unless is unique
+      $code = $faker->shuffle($faker->regexify('[A-Z]{4}[0-4]{8}'));
+      while (in_array($code, $existingCode)) {
+        $code = $faker->shuffle($faker->regexify('[A-Z]{4}[0-4]{8}'));
+      }
 
       $train = new Train();
 
@@ -40,10 +46,11 @@ class TrainSeeder extends Seeder
       $train->departure_time = $departTime;
       $train->arrival_date = $arrDate;
       $train->arrival_time = $arrTime;
-      // $train->code = "";
+      $train->code = $code;
       $train->wagons = $faker->numberBetween(5, 30);
       $train->on_time = $faker->numberBetween(0, 1);
       $train->cancelled = $faker->numberBetween(0, 1);
+
       $train->save();
     }
 
